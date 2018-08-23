9566e3c765acdf5f5dd7b3100213e37a05cefc79
Cristianx10 <Cristianx10@users.noreply.github.com>
2018-08-23 12:38:23 -0500

    Se programo la parte java de la actividad formularia

--- app/src/main/java/com/example/ecosistemas/ejercicios4_2/UserFormActivity.java
+++ app/src/main/java/com/example/ecosistemas/ejercicios4_2/UserFormActivity.java
@@ -1,13 +1,30 @@
 package com.example.ecosistemas.ejercicios4_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UserFormActivity extends AppCompatActivity {
+
+    private EditText edt_userform_name;
+    private EditText edt_userform_email;
+    private EditText edt_userform_userform;
+
+    private Button btn_userform_finish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_form);
+
+        Intent solicitud = getIntent();
+
+        edt_userform_name = findViewById(R.id.edt_userform_name);
+        edt_userform_email = findViewById(R.id.edt_userform_email);
+        edt_userform_userform = findViewById(R.id.edt_userform_nick);
+
+        btn_userform_finish = findViewById(R.id.btn_userform_finish);
     }
 }
